757901b97e3b9d6cb6c651907c33179173030f54
jaygon4146 <jaygon4146@gmail.com>
2018-04-18 12:04:35 +1000

    NeedToRevert

---
+++ UnityProject/GreatJerboaPrototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public PixelMapLoader MapLoader;
+
+	void Awake(){
+		MapLoader.Activate ();
+		MapLoader.LoadMap();
+		SpawnPlayerCharacter ();
+	}
+
+
+	void SpawnPlayerCharacter(){
+
+		Vector3 PCSpawn = MapLoader.getPCSpawn ();
+
+	}
+}
