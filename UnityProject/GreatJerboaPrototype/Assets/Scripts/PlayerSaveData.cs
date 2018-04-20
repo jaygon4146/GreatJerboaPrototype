fde43f1563ad26833e1ad4c9a312fc629dc222ab
jaygon4146 <jaygon4146@gmail.com>
2018-04-20 11:27:17 +1000

    Begin Serializable Save Data

---
+++ UnityProject/GreatJerboaPrototype/Assets/Scripts/PlayerSaveData.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSaveData {
+
+	private List<LevelSaveData> levelData = new List <LevelSaveData>();
+
+}
