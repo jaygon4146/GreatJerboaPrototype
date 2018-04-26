397c4f73bbd4aa159bca8721d500532e55fd5049
jaygon4146 <jaygon4146@gmail.com>
2018-04-26 11:33:08 +1000

    Cell Sprite/Collider Tiling

--- UnityProject/GreatJerboaPrototype/Assets/Scripts/CellObject.cs
+++ UnityProject/GreatJerboaPrototype/Assets/Scripts/CellObject.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class CellObject : MonoBehaviour {
 
 	public SpriteRenderer renderer;
-	public GameObject collider;
+	//public GameObject collider;
 
 	public void PassScale(Vector2 v){
 		renderer.size = v;
-		Vector3 scale = new Vector3 (v.x, v.y, 1);
-		collider.transform.localScale = scale;
+		//Vector3 scale = new Vector3 (v.x, v.y, 1);
+		//collider.transform.localScale = scale;
 	}
 
 }
